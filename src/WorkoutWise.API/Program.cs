9e7cb961fb706df2400230d74c7339f9e9e8f590
jaysonAviso <jayson.aviso1@gmail.com>
2025-05-15 02:05:23 +0800

    App Settings: Connection String

--- src/WorkoutWise.API/Program.cs
+++ src/WorkoutWise.API/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using WorkoutWise.Application;
+using WorkoutWise.Infrastructure;
+using WorkoutWise.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +10,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+    opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure();
 
 var app = builder.Build();
 
